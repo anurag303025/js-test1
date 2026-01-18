b61a68c0fec8029496b87287f1077682153b6a95
Anurag suryavanshi <bainsanurag821@gmail.com>
2026-01-18 15:44:45 +0000

    02

---
+++ 01_basic/01_datatype.js
@@ -0,0 +1,10 @@
+"use strict"; //treat all js code as newer version   
+
+//alert(3+3)// we are using node js not browser
+
+console.log(3+3);//code readebility code ko line wise likho
+
+let name="abhi"
+let age=21
+let isLoggedIn=false
+console.log(typeof isLoggedIn)
---
+++ 01_basic/variable.js
@@ -0,0 +1,10 @@
+const accountId=12345
+let accountEmail="anurag@123.gmail"
+var accountPass ="123456"
+accountCity="bhoa"
+let accountState;
+
+accountEmail="abhi@123"
+accountPass="123"
+accountCity="amritsar"
+console.table([accountEmail,accountPass,accountCity,accountId,accountState])
